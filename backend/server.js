682e2638c999e94e9d29aabcabc63228b1b8f270
Biruh <lebiruh@yahoo.com>
2022-07-22 10:57:27 +0300

    REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 const express = require("express");
+const cors = require('cors');
 const dotenv = require('dotenv').config();
-const errorHandler = require('./middleware/errorMiddleware')
+const mongoose = require('mongoose');
 const port = process.env.PORT;
+
+
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 
-app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(port, () => console.log(`Server is running on port: ${port}`)))
+.catch((error) => console.log(error.message));
+
