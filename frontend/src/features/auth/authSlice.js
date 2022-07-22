f9b38a7a1f630ece04af1db3e9a0575819014b70
Biruh <lebiruh@yahoo.com>
2022-07-22 19:29:38 +0300

    redux global state added

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,55 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import authService from './authService';
+
+const user = JSON.parse(localStorage.getItem('user'));
+
+export const register = createAsyncThunk('auth/register', async (user, thunckAPI) => {
+  try {
+    return await authService.register(user);
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+    return thunckAPI.rejectWithValue(message);
+  }
+})
+
+const initialState = {
+  user: user ? user : null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: ''
+}
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.isSuccess = false;
+      state.message = ''
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+    .addCase(register.pending, (state) => {
+      state.isLoading = true;
+    })
+    .addCase(register.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = true;
+      state.user = action.payload;
+    })
+    .addCase(register.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = false;
+      state.isError = true;
+      state.message = action.payload;
+      state.user = null
+    })
+  }
+})
+
+export const {reset} = authSlice.actions;
+export default authSlice.reducer;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,10 @@
 import React, {useState, useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+import {toast} from 'react-toastify';
 import { FaUser } from 'react-icons/fa';
+import { register, reset } from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -8,8 +13,25 @@ const Register = () => {
     password: '',
     confirmPassword: ''
   })
-
+  
   const {name, email, password, confirmPassword} = formData;
+  
+  // const navigate = useNavigate();
+  // const dispatch = useDispatch();
+
+  // const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth)
+
+  // useEffect(() => {
+  //   if(isError) {
+  //     toast.error(message);
+  //   }
+
+  //   if(isSuccess || user) {
+  //     navigate('/');
+  //   }
+
+  //   dispatch(reset());
+  // }, [user, isError, isSuccess, message, navigate, dispatch])
 
   const onChange = (e) => setFormData({
     ...formData,
@@ -18,7 +40,19 @@ const Register = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-  };
+
+    // if(password !== confirmPassword) {
+    //   toast.error('passwords do not match')
+    // } else {
+    //   const userData = {name, email, password}
+
+      // dispatch(register(userData))
+    }
+  // };
+
+  // if(isLoading) {
+  //   return <Spinner />
+  // }
 
   return (
     <>
